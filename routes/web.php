da7fd46831cfc8e8fc74b129b46f49e270f06688
Saiful Islam <codersaiful@gmail.com>
2021-11-02 06:54:16 +0600

    working

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('attendence');
 });
 
 Route::get('invoice',[InvoiceController::class,'index'])->name('invoice');
