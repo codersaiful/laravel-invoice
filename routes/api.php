7d926970be2c00bd37cbcba1af0a26e61494d289
Saiful Islam <codersaiful@gmail.com>
2021-11-04 08:17:50 +0600

    fff

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('hello',function(){
     return array('id'=>12,'staus'=>200);
 });
+Route::get('hello',function(){
+    return array('id'=>12,'staus'=>200);
+});
